4a63cbde1d6e95026b801d5e99f8b482b84e80a2
areebasaleem456 <areebasaleem771@gmail.com>
2025-02-18 01:56:37 +0500

    Add files via upload

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,15 +4,14 @@ import { useState } from "react";
 import React from "react";
 
 export default function AdminLogin(){
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const router = useRouter()
-    const handleLogin = (e:React.FormEvent) =>{
-        e.preventDefault()
-    }
-    if (email === "areebasaleem773@gmail.com"){
+    // const [email, setEmail] = useState("")
+    // const router = useRouter()
+    // const handleLogin = (e:React.FormEvent) =>{
+    //     e.preventDefault()
+    // }
+    // if (email === "areebasaleem773@gmail.com"){
    
-    }
+    // }
     return(
         <div>adminlogin</div>
     )
--- app/components/ProtectedRoute.tsx
+++ app/components/ProtectedRoute.tsx
@@ -1,19 +1,40 @@
+// "use client";
+// import { useRouter } from "next/navigation";
+// import React, { useEffect } from "react";
+
+// export default function ProtectedRoute({children}:{children:React.ReactNode}){
+//   const router = useRouter()
+//   useEffect(()=>{
+//     const isLoggedIn = localStorage.getItem("isLoggedIn")
+//     if(!isLoggedIn){
+//         router.push("/admin/dashboard")
+//     }
+//   },[router])
+//   return (
+//     <div>
+//         {children}
+//     </div>
+//   )
+// }
+
+
 "use client";
 import { useRouter } from "next/navigation";
-import React from "react";
-import { useEffect} from "react";
+import React, { useEffect } from "react";
 
-export default function ProtectedRoute({children}:{children:React.ReactNode}){
-  const router = useRouter()
-  useEffect(()=>{
-    const isLoggedIn = localStorage.getItem("isLoggedIn")
-    if(!isLoggedIn){
-        router.push("/admin/dashboard")
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (!isLoggedIn) {
+      router.push("/admin/dashboard");
     }
-  },[router])
+  }, [router]);
+
   return (
     <div>
-        {children}
+      {children}
     </div>
-  )
+  );
 }
